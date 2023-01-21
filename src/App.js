a2ba7f3811d2aaf4977da860e2abcb8c90dea86f
Johnsonnejo <johnsonnejo@gmail.com>
2023-01-21 05:05:28 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My first Docker work with React..
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Johnson Nejo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
